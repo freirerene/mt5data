9106122e0bb768a280f680d59186d3922ee54e80
Rene Freire <rene@freire.ch>
2024-12-29 16:38:56 -0300

    fix

--- MTserver/routers/history.py
+++ MTserver/routers/history.py
@@ -2,12 +2,17 @@ from utils.schemas import GetHistory
 from utils.mtfunctions import get_history
 
 from fastapi import APIRouter
+from typing import Optional
 
 router = APIRouter()
 
 
 @router.get("/history")
-async def buy_order(info_request: GetHistory):
-
+async def buy_order(
+    symbol: str, timeframe: str, ticks: Optional[int] = 0, from_date: Optional[str] = ""
+):
+    info_request = GetHistory(
+        symbol=symbol, timeframe=timeframe, ticks=ticks, from_date=from_date
+    )
     df = get_history(info_request)
-    return df.to_dict(orient="records")
+    return df
